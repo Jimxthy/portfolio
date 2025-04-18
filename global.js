ad950806867cb6bfa4b72279cde2aaf768c19acd
Jimxthy <jih103@ucsd.edu>
2025-04-17 18:44:05 -0700

    changed nav

--- global.js
+++ global.js
@@ -4,11 +4,29 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$("nav a")
+// const navLinks = $$("nav a")
 
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname,
-  );
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname,
+//   );
 
-  currentLink.classList.add('current');
+//   currentLink.classList.add('current');
+  
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: "https://github.com/Jimxthy", title: Github},
+    { url: "resume/", title: Resume}
+  ];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+
+  }
   
