c9f07271fee9c7e349e465154fef5171fe485518
Jimxthy <jih103@ucsd.edu>
2025-04-17 19:02:41 -0700

    updated links

--- global.js
+++ global.js
@@ -12,21 +12,23 @@ function $$(selector, context = document) {
 
 //   currentLink.classList.add('current');
   
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
+
 let pages = [
-    { url: '', title: 'Home' },
-    { url: 'projects/', title: 'Projects' },
-    { url: 'contact/', title: 'Contact' },
-    { url: "https://github.com/Jimxthy", title: 'Github'},
-    { url: "resume/", title: 'Resume'}
-  ];
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: "https://github.com/Jimxthy", title: 'Github' },
+  { url: "resume/", title: 'Resume' }
+];
 
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
 for (let p of pages) {
-    let url = p.url;
-    let title = p.title;
-    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
-
-  }
-  
+  let url = p.url.startsWith('http') ? p.url : BASE_PATH + p.url;
+  let title = p.title;
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
